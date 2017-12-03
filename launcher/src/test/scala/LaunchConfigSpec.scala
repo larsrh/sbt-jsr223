28323d3b1942ac67a065921ff44a8d02f40698c0
Mike Slinn <mslinn@micronauticsresearch.com>
2017-12-02 17:38:16 -0800

    -

--- launcher/src/test/scala/LaunchConfigSpec.scala
+++ launcher/src/test/scala/LaunchConfigSpec.scala
@@ -6,7 +6,6 @@ import javax.script.ScriptException
 import scala.collection.JavaConverters._
 import scala.util.Try
 import org.specs2.Specification
-import org.specs2.matcher.MatchResult
 
 class LaunchConfigSpec extends Specification { def is = s2"""
 
@@ -17,16 +16,16 @@ class LaunchConfigSpec extends Specification { def is = s2"""
 
   """
 
-  def launchConfig(script: String): LaunchConfig =
+  def launchConfig(script: String) =
     LaunchConfig.ofResource("python", getClass.getClassLoader, script, Charset.forName("UTF-8"))
 
-  def launch: MatchResult[AnyRef] =
+  def launch =
     launchConfig("test_launch.py").launch(Map[String, Object]("y" -> (1: Integer)).asJava).get("x") must beEqualTo(4)
 
-  def cp: MatchResult[AnyRef] =
+  def cp =
     launchConfig("test_cp.py").launch(Collections.emptyMap()).get("value") must beEqualTo(TestObject.hi)
 
-  def fails: MatchResult[Try[Unit]] =
+  def fails =
     Try(launchConfig("test_fail.py").launch()) must beFailedTry[Unit].withThrowable[ScriptException](".*NameError.*")
 
 }
